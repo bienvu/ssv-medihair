6b4b765308f465b415630a8ca85edf854560b5ad
sonha807 <sonha807@gmail.com>
2019-01-08 11:57:00 +0700

    MED update layout grid image, cta

--- wp-content/themes/medihair/templates/components/grid-image.php
+++ wp-content/themes/medihair/templates/components/grid-image.php
@@ -6,41 +6,55 @@ if( get_row_layout() == 'grid_image' ):
   $gridImageItem = get_sub_field('grid_image_item');
   ?>
   
-  <div class="grid-image <?php echo $type; ?>">
-    <?php if($blockTitle): ?>
-      <h2 class="section-title text--center"><?php echo $blockTitle; ?></h2>
+  <div class="grid-image <?php echo $type; ?> <?php if($type=='grid-image--2col'){echo 'how-it-works';} ?>">
+    <?php if($type=='grid-image--2col'): ?>
+    <div class="container">
     <?php endif; ?>
-    <?php if($description): ?>
-      <p><?php echo $description; ?></p>
-    <?php endif; ?>
-    <div class="grid-image__inner">
-      <div class="grid-image__wrap">
-        <?php 
-        if( have_rows('grid_image_item') ):
-        // loop through the rows of data
-          while ( have_rows('grid_image_item') ) : the_row();
-            // Field variables
-            $image = get_sub_field('image');
-            $content = get_sub_field('content');
-            $link = get_sub_field('link'); ?>
+      <?php if($blockTitle): ?>
+        <div class="section-title">
+          <h2><?php echo $blockTitle; ?></h2>
+          <?php if($description): ?>
+            <p><?php echo $description; ?></p>
+          <?php endif; ?>
+        </div>  
+      <?php endif; ?>
+      <div class="grid-image__inner">
+        <div class="grid-image__wrap">
+          <?php 
+          if( have_rows('grid_image_item') ):
+          // loop through the rows of data
+            while ( have_rows('grid_image_item') ) : the_row();
+              // Field variables
+              $image = get_sub_field('image');
+              $content = get_sub_field('content');
+              $link = get_sub_field('link'); ?>
 
-            <div class="grid-image__item">
-              <div class="grid-image__content">
-                <a href="<?php echo $link['url']; ?>">
-                  <?php echo wp_get_attachment_image( $image['ID'], 'full' ); ?>
-                  
-                </a>
-                <div class="grid-image__link text--white ">
-                  <div class="grid-image__body">
-                    <?php echo $content; ?>
+              <div class="grid-image__item">
+                <div class="grid-image__content">
+                  <a href="<?php echo $link['url']; ?>">
+                    <?php echo wp_get_attachment_image( $image['ID'], 'full' ); ?>
+                    <?php if($type=='grid-image--4col'): ?>
+                      <div class="grid-image--4col__content">
+                        <?php echo $content; ?>
+                      </div>  
+                    <?php endif; ?> 
+                  </a>
+                  <div class="grid-image__link">
+                    <?php if($type=='grid-image--2col'): ?>
+                      <div class="grid-image__body">
+                        <?php echo $content; ?>
+                      </div>
+                    <?php endif; ?>
+                    <a class="btn btn--bg-white" href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>"><?php echo $link['title']; ?></a>
                   </div>
-                  <a class="btn btn--bg-white" href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>">View results</a>
                 </div>
               </div>
-            </div>
-          <?php endwhile;
-        endif; ?>
+            <?php endwhile;
+          endif; ?>
+        </div>
       </div>
-    </div>
+    <?php if($type=='grid-image--2col'): ?>  
+    </div>  
+    <?php endif; ?>
   </div>
 <?php endif; ?>
