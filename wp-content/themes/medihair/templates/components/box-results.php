e2b3c395242ce8482e982e6495b9b781e5e6e3b7
sonha807 <sonha807@gmail.com>
2019-01-09 10:14:43 +0700

    MED update rusults page

--- wp-content/themes/medihair/templates/components/box-results.php
+++ wp-content/themes/medihair/templates/components/box-results.php
@@ -3,12 +3,13 @@ if( get_row_layout() == 'box_results' ):
   $boxResultsItem = get_sub_field('box_results_item');
   $title = get_sub_field('block_title');
   $description = get_sub_field('description');
+  $links = get_sub_field('links');
   ?>
 
   <div class="box-results">
     <div class="container">
       <div class="section-title text--left">
-          <h2><?php echo $title; ?></h2>
+          <h1><?php echo $title; ?></h1>
           <?php echo $description; ?>
         </div>
       <div class="box-results__wrap">
@@ -31,7 +32,22 @@ if( get_row_layout() == 'box_results' ):
                 <?php endif; ?>
                 </div>
             <?php endwhile;
-          endif; ?>    
+          endif; ?>
+      </div>
+      <div class="box-results__link">
+        <div class="multiLink">
+          <?php 
+          if( have_rows('links') ):
+          // loop through the rows of data
+            while ( have_rows('links') ) : the_row();
+              // Field variables
+              $link = get_sub_field('link'); ?>
+
+               <a class="btn" href="<?php print $link['url'] ?>"><?php print $link['title'] ?></a>
+              
+             <?php endwhile;
+          endif; ?>
+        </div>
       </div>
     </div>
   </div>
