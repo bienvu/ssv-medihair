2dece495350123e1981017cf81766a4bbb9bfcd2
sonha807 <sonha807@gmail.com>
2019-01-10 13:51:13 +0700

    MED update test html space

--- wp-content/themes/medihair/category.php
+++ wp-content/themes/medihair/category.php
@@ -1,37 +1,37 @@
 <?php get_header(); ?>
 <?php
-	$termId = get_queried_object()->term_id;
-	$childrenData = get_term_children($termId, 'category');
+  $termId = get_queried_object()->term_id;
+  $childrenData = get_term_children($termId, 'category');
 ?>
-	<main role="main" class="<?php if($childrenData):?>sub-category-page<?php else: ?>category-page<?php endif; ?>">
-		<div class="list-article">
-			<div class="container">
-				<div class="section-title">
-		      <h2><?php printf(single_cat_title( '', false ) ); ?></h2>
-		    </div>
+  <main role="main" class="<?php if($childrenData):?>sub-category-page<?php else: ?>category-page<?php endif; ?>">
+    <div class="list-article">
+      <div class="container">
+        <div class="section-title">
+          <h2><?php printf(single_cat_title( '', false ) ); ?></h2>
+        </div>
 
-		    <div class="list-article__wrap">
-		    <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-		    		<div class="list-article__item">
-		    			<div class="list-article__image">
-		    				<a href="<?php the_permalink(); ?>">
-			    				<?php
-										if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-										  the_post_thumbnail();
-										}
-									?>
-								</a>
-		    			</div>
-		    			<div class="list-article__content">
-		    				<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-		    				<?php the_excerpt(); ?>
-		    			</div>
-		    		</div>
-		   		<?php endwhile;
-		   	endif; ?>
-		   	</div> 	
-		   	<?php get_template_part('templates/pagination'); ?>
+        <div class="list-article__wrap">
+        <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+            <div class="list-article__item">
+              <div class="list-article__image">
+                <a href="<?php the_permalink(); ?>">
+                  <?php
+                    if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+                      the_post_thumbnail();
+                    }
+                  ?>
+                </a>
+              </div>
+              <div class="list-article__content">
+                <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+                <?php the_excerpt(); ?>
+              </div>
+            </div>
+          <?php endwhile;
+        endif; ?>
+        </div>  
+        <?php get_template_part('templates/pagination'); ?>
       </div>  
-		</div>    
-	</main>
+    </div>    
+  </main>
 <?php get_footer(); ?>
